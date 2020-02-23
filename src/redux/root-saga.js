bbb6701f181ba1fac7b09efc1f333f2a7e947822
ttcode10 <ttcode10@gmail.com>
2020-02-23 20:07:42 +0800

    Removed Redux Saga

--- src/redux/root-saga.js
+++
@@ -1,9 +0,0 @@
-import { all, call } from 'redux-saga/effects';
-
-import { todoSaga } from './todo/todo.saga';
-
-export default function* rootSaga() {
-  yield all([
-    call(todoSaga)
-  ])
-}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
-import rootSaga from './root-saga';
 
-const sagaMiddleware = createSagaMiddleware();
-
-const middlewares = [logger, sagaMiddleware];
+const middlewares = [logger];
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
-
-sagaMiddleware.run(rootSaga);
 
 export default store;
